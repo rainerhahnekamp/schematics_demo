ebf828c004acf813f1320da48d8ad068e9670bfd
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2024-06-23 23:45:58 +0200

    v6

--- ngc/src/migrations/v0_0_4/index.ts
+++ ngc/src/migrations/v0_0_4/index.ts
@@ -1,7 +1,7 @@
 import {Rule} from "@angular-devkit/schematics";
 import {updatePlaywright} from "../../util/update-playwright";
 
-export function update_to_0_0_3(): Rule {
+export default function update_to_0_0_3(): Rule {
   return (tree, _context) => {
     updatePlaywright(tree, _context, '1.42');
   }
